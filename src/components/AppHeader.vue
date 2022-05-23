10412253ad20e0cc6291063ce53b3fe2a6bc51cc
alexcapoluongo <alexcapoluongo@icloud.com>
2022-05-23 16:48:20 +0200

    trying edit axios on log

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,31 @@
 <template>
   <header>
-      <input type="text">
+      <input type="text" v-model= "inputUser" >
+      <button @click="modifyAxios" >Search film</button>
   </header>
 </template>
 
 <script>
+
 export default {
     name: "AppHeader",
+    data: function() {
+        return {
+        inputUser: "",
+        }
+    },
+    methods: {
+        
+        modifyAxios: function() {
+            this.movieSearched = this.inputUser
+            console.log(this.movieSearched);
+            this.inputUser="";
+         
+        }
+
+    }
 }
+
 </script>
 
 <style>
