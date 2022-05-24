e0e75029faaac0b892912ef89d633795d9aa67aa
alexcapoluongo <alexcapoluongo@icloud.com>
2022-05-24 11:35:32 +0200

    demo flag languages

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,21 +1,38 @@
 <template>
     <div class="container">
        
-        <div> {{ movie.title }} </div>
-        <div> {{ movie.original_title }} </div>
-        <div> {{ movie.original_language }} </div>
-        <div> {{ movie.vote_average }} </div>
+        <div class="title"> Title : {{ movie.title }} </div>
+        <div class="original-title"> Original:  {{ movie.original_title }} </div>
+        <div class="language"> 
+            Language : {{ movie.original_language }} <img :src="image" alt="" srcset="">
+         </div>
+        <div class="vote"> Vote : {{ movie.vote_average }} </div>
         
       
     </div>
 </template>
 
 <script>
+import imageEn from "../assets/img/en.png"
+import imageFr from "../assets/img/fr.png"
+import imageIt from "../assets/img/it.png"
+import imageWr from "../assets/img/zz.png"
+
 export default {
     name: "AppCard",
     props: {
         movie: Object
     },
+
+    data() {
+        return{
+            nameLanguage: "",
+            image: imageEn
+        }
+    },
+
+    methods: {
+    }
 }
 </script>
 
@@ -26,5 +43,6 @@ export default {
     width: 100px;
     margin: 10px
 }
+
 
 </style>
